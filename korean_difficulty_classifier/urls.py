c921deda19d03322e6dbc892fdbf68cb6ed653dc
owainjhughes <owainjhughes@gmail.com>
2026-01-09 16:18:05 +0000

    fix: merge conflict

--- korean_difficulty_classifier/urls.py
+++ korean_difficulty_classifier/urls.py
@@ -6,9 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('classifier.urls')),
     path("login/", auth_views.LoginView.as_view(), name="login"),
-<<<<<<< HEAD
     path("logout/", auth_views.LogoutView.as_view(), name="logout")
-=======
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
->>>>>>> 225454725c60328a89aae8f7f61e4c0200fd66d3
 ]
